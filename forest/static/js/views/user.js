f10a0820ce44eb8bbd11b97141281fdc1efb2d94
Sean McLean <seanm@apple.com>
2019-02-11 17:40:19 -0800

    tweaks

--- forest/static/js/views/user.js
+++ forest/static/js/views/user.js
@@ -14,7 +14,7 @@ define(
                 'input_first_name': 'input[name=first_name]',
                 'input_last_name': 'input[name=last_name]',
                 'input_email': 'input[name=email]',
-                'password_change_form_result': 'div#password_change_form_result'
+                'user_form_result': 'div#user_form_result'
             },
 
             events: {
@@ -55,8 +55,8 @@ define(
                 });
             },
 
-            set_usersave_error: function(err) {
-                this.$el.find(this.elements.usersaveresult).html(err);
+            set_user_form_result: function(err) {
+                this.$el.find(this.elements.user_form_result).html(err);
             },
 
             user_form_save: function() {
@@ -64,7 +64,7 @@ define(
                 var passwd = this.$el.find(this.elements.chg_password).val();
                 var passwd_confirm = this.$el.find(this.elements.chg_password_confirm).val();
                 if (passwd && passwd != passwd_confirm) {
-                    this.set_password_error('Passwords do not match.');
+                    this.set_user_form_result('Passwords do not match');
                 } else {
                     if (passwd) {
                         this.user.set('password', passwd);
@@ -86,7 +86,7 @@ define(
                                 self.render();
                             },
                             error: function(err, resp) {
-                                self.set_usersave_error('Failed to change password: ' + resp.statusText);
+                                self.set_user_form_result('Failed to change password: ' + resp.statusText);
                             }
                         });
                 }
